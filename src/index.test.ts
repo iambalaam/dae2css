4871495bc3830ccc63fe734a9bb5ffd205c99788
Guy Balaam <guy.balaam@gmail.com>
2021-01-28 00:10:56 +0000

    Extracting triangle data

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,18 @@
-import { DAEFile2JSON, COLLADA } from './index';
+import {
+    DAEFile2JSON, JSON2Mesh, getTriangleIndecies,
+    COLLADA
+} from './index';
 
 let json;
 
 it('can parse xml attributes', async () => {
-    const dae = await DAEFile2JSON('mocks/icosphere.dae');
+    const dae = await DAEFile2JSON('mocks/cube.dae');
     json = dae;
     expect(Object.keys(dae)).toEqual([COLLADA]);
+});
+
+it('gets triangle data as vertex indices', () => {
+    expect(
+        getTriangleIndecies(JSON2Mesh(json)).length
+    ).toBeGreaterThan(2);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as assert from 'assert';
 import { resolve } from 'path';
 import { readFileSync } from 'fs';
 import { Parser } from 'xml2js';
@@ -9,6 +10,19 @@ export const GEOMETRY = 'geometry';
 export const MESH = 'mesh';
 export const SOURCE = 'source';
 
+export type Mesh = {
+    source: { $: { id: string } }[],
+    vertices: [{
+        $: { id: string },
+        input: { $: { source: string } }[]
+    }],
+    triangles: {
+        $: { id: string, count: string },
+        input: { $: { source: string, semantic: string, offset: string } }[],
+        p: string[]
+    }[]
+};
+
 export type ColladaJSON = {
     COLLADA: {
         $: {
@@ -18,15 +32,20 @@ export type ColladaJSON = {
         },
         asset: unknown,
         library_images: unknown,
-        library_geometries: [{
-            geometry: [{
-                mesh: unknown;
-            }]
-        }],
+        library_geometries: {
+            geometry: {
+                mesh: Mesh[];
+            }[]
+        }[],
         library_visual_scenes: unknown,
         scene: unknown
     }
-}
+};
+
+export type Vector3d = { x: number, y: number, z: number };
+export type Triangle3D = [Vector3d, Vector3d, Vector3d];
+export type TrianglesIndices = number[];
+export type Triangles3D = Triangle3D[];
 
 export async function DAEFile2JSON(relativePath: string) {
     // Read file
@@ -39,12 +58,52 @@ export async function DAEFile2JSON(relativePath: string) {
     }
 
     // Parse XML
-    let xml;
+    let xmlJSON;
     try {
-        xml = await new Parser(/* options */).parseStringPromise(contents);
+        xmlJSON = await new Parser(/* options */).parseStringPromise(contents);
     } catch (e) {
         throw new Error('Cannot parse XML');
     }
 
-    return xml;
-}
+    return xmlJSON;
+};
+
+export function JSON2Mesh(daeJson: ColladaJSON) {
+    const geometries = daeJson.COLLADA.library_geometries;
+    if (geometries.length > 1) {
+        throw new Error('File contains multiple geometries');
+    }
+    const meshes = geometries[0].geometry;
+    if (meshes.length > 1) {
+        throw new Error('File contains multiple meshes');
+    }
+
+    const mesh = meshes[0].mesh[0];
+    return mesh;
+};
+
+export function getTriangleIndecies(mesh: Mesh) {
+    // Get correct mapping
+    const verticesId = '#' + mesh.vertices[0].$.id;
+
+    // Find Vertex Source
+    const triangles = mesh.triangles[0];
+    const triangleCount = parseInt(triangles.$.count);
+    const vertexInput = triangles.input.find((input) => input.$.semantic === 'VERTEX');
+    const vertexSourceId = vertexInput.$.source;
+
+    if (vertexSourceId !== verticesId) {
+        throw new Error('Cannot find vertex data');
+    }
+
+    // Filter vertex data
+    const allTriangleData = triangles.p[0]
+        .split(/\s/)
+        .map((s) => parseInt(s));
+    const totalOffsets = allTriangleData.length / triangleCount / 3;
+    assert.strictEqual(Math.floor(totalOffsets), totalOffsets, 'Malformed vertex data'); // Must be integer
+    const offset = parseInt(vertexInput.$.offset);
+    const vertexTriangleData = allTriangleData.filter((_, i) => i % totalOffsets === offset);
+
+    return vertexTriangleData as TrianglesIndices;
+};
