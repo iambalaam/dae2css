3a90bc984f997dd7b44466b5f5356a44a80ef5d7
Guy Balaam <guy.balaam@gmail.com>
2021-02-04 15:45:22 +0000

    Fixing public API

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,27 @@ import { rotateTriangleWithHypFirst, getBoxModel, getAffineTransformationMatrix,
 import { renderCSSTriangle } from './html';
 import { Triangle3D } from './types';
 
-export async function renderCSSTriangles(relativePath: string, scale: number, className: string) {
+export async function renderCSSTriangles(relativePath: string, scale: number, className: string): Promise<string> {
     const triangles = (await parseDAETriangles(relativePath))
-            .map(rotateTriangleWithHypFirst)
-            .map((targetTriangle) => {
-                // scale up to px space
-                const scaled = scale3x3(scale, targetTriangle);
+        .map(rotateTriangleWithHypFirst)
+        .map((targetTriangle) => {
+            // scale up to px space
+            const scaled = scale3x3(scale, targetTriangle);
 
-                const box = getBoxModel(scaled);
-                const initialTriangle: Triangle3D = [
-                    { x: 0, y: 0, z: 0 },
-                    { x: box.width, y: 0, z: 0 },
-                    { x: box.borderLeft, y: -box.height, z: 0 }
-                ];
-                const matrixTransform = getAffineTransformationMatrix(initialTriangle, scaled);
-                const cssMatrixTransform = matrixTransform
-                    .map(({ x, y, z }, i) => `${x}, ${y}, ${z}, ${i < 3 ? 0 : 1}`)
-                    .join(',')
+            const box = getBoxModel(scaled);
+            const initialTriangle: Triangle3D = [
+                { x: 0, y: 0, z: 0 },
+                { x: box.width, y: 0, z: 0 },
+                { x: box.borderLeft, y: -box.height, z: 0 }
+            ];
+            const matrixTransform = getAffineTransformationMatrix(initialTriangle, scaled);
+            const cssMatrixTransform = matrixTransform
+                .map(({ x, y, z }, i) => `${x}, ${y}, ${z}, ${i < 3 ? 0 : 1}`)
+                .join(',')
 
-                return renderCSSTriangle(box.height, box.borderLeft, box.borderRight, `matrix3d(${cssMatrixTransform})`, className);
-            });
-        return triangles;
+            return renderCSSTriangle(box.height, box.borderLeft, box.borderRight, `matrix3d(${cssMatrixTransform})`, className);
+        });
+    return triangles.join('');
 }
 
 export * from './html';
